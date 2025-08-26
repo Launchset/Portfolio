c62f402bdb060f36dc0ad203d28868c8f299dc6e
jolo2745 <jhelyar04@gmail.com>
2025-08-26 15:37:44 +0100

    Refactor: remove unused contact API and package.json; add Google Analytics page tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ Required
+
 import App from './App';
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* 👈 You need this */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
